45e94019d8bdd7422d9300d17b13c2ebf17eb3f6
vagnermaltauro <vagnermaltauro@gmail.com>
2024-10-19 15:15:56 -0400

    feat: add type definitions for request body in json function
    - Added `BodyLeaf` and `Body` type definitions to handle nested body types
    - Updated `json` function to assert `req.body` as `Body`

--- src/tests/json.ts
+++ src/tests/json.ts
@@ -1,4 +1,8 @@
 import type { NextFunction, Request, Response } from "express";
 
+type BodyLeaf = Record<string, string | number>;
+type Body = | Record<string, string | number | BodyLeaf>;
+
 export function json(req: Request, res: Response, next: NextFunction) {
+    const body = req.body as Body
 }
