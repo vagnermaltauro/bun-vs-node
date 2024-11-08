dbdbb8b03d63caf99ffe0d0e7cf52328b7bdc4a2
vagnermaltauro <vagnermaltauro@gmail.com>
2024-11-08 16:46:27 -0300

    feat(tests): add structuredCopy function for deep cloning

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,3 +1,4 @@
 export { json } from './json';
 export { empty } from './empty';
-
+export { jsonCopy } from './json-copy';
+export { structuredCopy } from './structured-clone';
---
+++ src/tests/structured-copy.ts
@@ -0,0 +1,6 @@
+import { Request } from "express";
+
+export async function structuredCopy(req: Request): Promise<string> {
+    return JSON.stringify(structuredClone(req.body));
+}
+
